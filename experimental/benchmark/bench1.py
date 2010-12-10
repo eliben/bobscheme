5704d33ed96232a98c50507acdc93fb30cd2c019
eli.bendersky <devnull@localhost>
2010-12-10 08:51:52 +0200

    added barevm to benchmarking

--- experimental/benchmark/bench1.py
+++ experimental/benchmark/bench1.py
@@ -6,10 +6,13 @@
 # Eli Bendersky (eliben@gmail.com)
 # This code is in the public domain
 #-------------------------------------------------------------------------------
-import sys
+import sys, os
+from subprocess import Popen, PIPE
+import tempfile
+sys.path.insert(0, '../..')
 
-sys.path.insert(0, '..')
 from bob.utils import Timer
+from bob.bytecode import Serializer
 from bob.interpreter import interpret_code
 from bob.compiler import compile_code
 from bob.vm import BobVM
@@ -24,6 +27,26 @@ def interpreter_runner(code, ostream):
     interpret_code(code, output_stream=ostream)
 
 
+barevm_path = "../barevm/barevm.exe"
+def barevm_runner(code, ostream):
+    codeobject = compile_code(code)
+    serialized = Serializer().serialize_bytecode(codeobject)
+
+    # Get a temporary filename and write the serialized codeobject
+    # into it
+    #
+    fileobj, filename = tempfile.mkstemp()
+    os.write(fileobj, serialized)
+    os.close(fileobj)
+
+    vm_proc = Popen([barevm_path, filename], stdout=PIPE)
+    vm_output = vm_proc.stdout.read()
+
+    if ostream:
+        ostream.write(vm_output)
+    os.remove(filename)
+
+
 code_str = '''
 (define (divides k n)
     (= (modulo n k) 0))
@@ -36,16 +59,18 @@ code_str = '''
             (else (auxprimecheck (+ 1 divisor)))))
     (auxprimecheck 2))
 
-(write (primecheck 2711))  ; it's a prime!
+(write (primecheck 59053))  ; it's a prime!
 '''
 
 # Since the interpreter isn't tail-recursive, this is required
 sys.setrecursionlimit(200000)
 
-with Timer('interp'):
-    interpreter_runner(code_str, None)
+#with Timer('interp'):
+    #interpreter_runner(code_str, None)
 
 with Timer('comp_vm'):
     vm_compiler_runner(code_str, None)
 
+with Timer('barevm'):
+    barevm_runner(code_str, None)
 
