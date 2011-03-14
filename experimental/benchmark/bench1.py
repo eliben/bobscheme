9b33a0bf884303f458b02df911f1e3f2d4854d1f
Eli Bendersky <eliben@gmail.com>
2011-03-14 13:52:00 +0200

    split benchmark common code to utils file

--- experimental/benchmark/bench1.py
+++ experimental/benchmark/bench1.py
@@ -9,42 +9,12 @@
 import sys, os
 from subprocess import Popen, PIPE
 import tempfile
+from utils import vm_compiler_runner, interpreter_runner, make_barevm_runner
+
 sys.path.insert(0, '../..')
-
 from bob.utils import Timer
-from bob.bytecode import Serializer
-from bob.interpreter import interpret_code
-from bob.compiler import compile_code
-from bob.vm import BobVM
-
-
-def vm_compiler_runner(code, ostream):
-    codeobject = compile_code(code)
-    vm = BobVM(output_stream=ostream)
-    vm.run(codeobject)
-
-def interpreter_runner(code, ostream):
-    interpret_code(code, output_stream=ostream)
-
 
 barevm_path = "../barevm/barevm.exe"
-def barevm_runner(code, ostream):
-    codeobject = compile_code(code)
-    serialized = Serializer().serialize_bytecode(codeobject)
-
-    # Get a temporary filename and write the serialized codeobject
-    # into it
-    #
-    fileobj, filename = tempfile.mkstemp()
-    os.write(fileobj, serialized)
-    os.close(fileobj)
-
-    vm_proc = Popen([barevm_path, filename], stdout=PIPE)
-    vm_output = vm_proc.stdout.read()
-
-    if ostream:
-        ostream.write(vm_output)
-    os.remove(filename)
 
 
 code_str = '''
@@ -59,7 +29,7 @@ code_str = '''
             (else (auxprimecheck (+ 1 divisor)))))
     (auxprimecheck 2))
 
-(write (primecheck 59053))  ; it's a prime!
+(write (primecheck 5903))  ; it's a prime!
 '''
 
 # Since the interpreter isn't tail-recursive, this is required
@@ -72,5 +42,5 @@ with Timer('comp_vm'):
     vm_compiler_runner(code_str, None)
 
 with Timer('barevm'):
-    barevm_runner(code_str, None)
+    make_barevm_runner(barevm_path)(code_str, None)
 
---
+++ experimental/benchmark/utils.py
@@ -0,0 +1,38 @@
+import sys, os
+from subprocess import Popen, PIPE
+import tempfile
+sys.path.insert(0, '../..')
+from bob.bytecode import Serializer
+from bob.interpreter import interpret_code
+from bob.compiler import compile_code
+from bob.vm import BobVM
+
+def vm_compiler_runner(code, ostream):
+    codeobject = compile_code(code)
+    vm = BobVM(output_stream=ostream)
+    vm.run(codeobject)
+
+
+def interpreter_runner(code, ostream):
+    interpret_code(code, output_stream=ostream)
+
+
+def make_barevm_runner(barevm_path):
+    def barevm_runner(code, ostream):
+        codeobject = compile_code(code)
+        serialized = Serializer().serialize_bytecode(codeobject)
+
+        # Get a temporary filename and write the serialized codeobject
+        # into it
+        #
+        fileobj, filename = tempfile.mkstemp()
+        os.write(fileobj, serialized)
+        os.close(fileobj)
+
+        vm_proc = Popen([barevm_path, filename], stdout=PIPE)
+        vm_output = vm_proc.stdout.read()
+
+        if ostream:
+            ostream.write(vm_output)
+        os.remove(filename)
+    return barevm_runner
