dc94f1d2c413da7c5c2fc91ea8e2291015621531
Eli Bendersky <eliben@gmail.com>
2025-11-03 17:53:42 -0800

    Modernize scripts in examples/...

--- examples/interpret_file.py
+++ examples/interpret_file.py
@@ -1,8 +1,6 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import sys
-
-sys.path.insert(0, '..')
 from bob.cmd import interpret_file
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     interpret_file()
