664869d9c16fc0ad87aa7208d6e111cae83f4f7e
Eli Bendersky <eliben@gmail.com>
2026-01-08 06:25:01 -0800

    Remove some old Python 2 compat code, and reformat some files

--- bob/utils.py
+++ bob/utils.py
@@ -1,11 +1,11 @@
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 # utils.py
 #
 # Some generic utilities
 #
 # Eli Bendersky (eliben@gmail.com)
 # This code is in the public domain
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 from __future__ import print_function
 from collections import deque
 import struct
@@ -13,18 +13,18 @@ import time
 
 
 def flatten(iterables):
-    """ Flatten an iterable of iterables. Returns a generator.
+    """Flatten an iterable of iterables. Returns a generator.
 
-        list(flatten([[2, 3], [5, 6]])) => [2, 3, 5, 6]
+    list(flatten([[2, 3], [5, 6]])) => [2, 3, 5, 6]
     """
     return (elem for iterable in iterables for elem in iterable)
 
 
 def list_find_or_append(lst, item):
-    """ If item is in the list, return its index. Otherwise, append it to the
-        list and return its index.
+    """If item is in the list, return its index. Otherwise, append it to the
+    list and return its index.
 
-        Note: may modify the list
+    Note: may modify the list
     """
     try:
         return lst.index(item)
@@ -34,42 +34,28 @@ def list_find_or_append(lst, item):
 
 
 def pack_word(word, big_endian=False):
-    """ Packs a 32-bit word into a binary data string.
-    """
+    """Packs a 32-bit word into a binary data string."""
     endian = ">" if big_endian else "<"
     return struct.pack("%sL" % endian, word)
 
 
 def unpack_word(str, big_endian=False):
-    """ Unpacks a 32-bit word from binary data.
-    """
+    """Unpacks a 32-bit word from binary data."""
     endian = ">" if big_endian else "<"
     return struct.unpack("%sL" % endian, str)[0]
 
 
-def byte_literal(b):
-    """ If b is already a byte literal, return it. Otherwise, b is
-        an integer which should be converted to a byte literal.
-
-        This function is for compatibility with Python 2.6 and 3.x
-    """
-    if isinstance(b, int):
-        return bytes([b])
-    else:
-        return b
-
-
 def get_bytes_from_iterator(it, nbytes):
-    """ Takes a few bytes from an iterator and returns them as a single
-        bytes object.
+    """Takes a few bytes from an iterator and returns them as a single
+    bytes object.
     """
     lst = [next(it) for n in range(nbytes)]
-    return b''.join(byte_literal(b) for b in lst)
+    return bytes(lst)
 
 
 class Stack(object):
-    """ A simple stack abstraction.
-    """
+    """A simple stack abstraction."""
+
     def __init__(self):
         self.d = deque()
 
@@ -77,17 +63,17 @@ class Stack(object):
         self.d.append(item)
 
     def pop(self):
-        """ Pop and return element at top-of-stack.
-            Raise IndexError if the stack is empty.
+        """Pop and return element at top-of-stack.
+        Raise IndexError if the stack is empty.
         """
         return self.d.pop()
 
     def peek(self, index=-1):
-        """ Peek at some element of the stack without removing it. By default
-            the element at the top-of-stack is returned. Pass a negative
-            index similar to Python list syntax for arbitrary peeking.
+        """Peek at some element of the stack without removing it. By default
+        the element at the top-of-stack is returned. Pass a negative
+        index similar to Python list syntax for arbitrary peeking.
 
-            Raise IndexError for invalid indices.
+        Raise IndexError for invalid indices.
         """
         return self.d[index]
 
@@ -104,5 +90,5 @@ class Timer(object):
 
     def __exit__(self, type, value, traceback):
         if self.name:
-            print('[%s]' % self.name)
-        print('Elapsed: %s' % (time.time() - self.tstart))
+            print("[%s]" % self.name)
+        print("Elapsed: %s" % (time.time() - self.tstart))
--- tests_full/testcases_utils.py
+++ tests_full/testcases_utils.py
@@ -1,4 +1,4 @@
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 # bob: tests_full/testcases_utils.py
 #
 # Utilities for loading test cases from the testcases/ directory and running
@@ -6,10 +6,10 @@
 #
 # Eli Bendersky (eliben@gmail.com)
 # This code is in the public domain
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 from __future__ import print_function
 import os, time, sys
-from io import StringIO
+import io
 
 
 class TestCase(object):
@@ -19,30 +19,23 @@ class TestCase(object):
         self.expected = expected
 
 
-def all_testcases(dir='tests_full/testcases'):
+def all_testcases(dir="tests_full/testcases"):
     for filename in sorted(os.listdir(dir)):
-        if filename.endswith('.scm'):
+        if filename.endswith(".scm"):
             testname = os.path.splitext(filename)[0]
             fullpath = os.path.join(dir, filename)
             code = open(fullpath).read()
 
-            exp_path = os.path.join(dir, testname + '.exp.txt')
+            exp_path = os.path.join(dir, testname + ".exp.txt")
             expected = open(exp_path).read()
 
             yield TestCase(testname, code, expected)
 
 
-class StringIOUnicode(StringIO):
-    """ For compatibility with both Python 2.6 and 3.x
-    """
-    def write(self, s):
-        StringIO.write(self, s)
-
-
-def run_all_tests(runner, dir='testcases/'):
-    """ Runs all tests from the directory with the given runner. A runner
-        is a function accepting Scheme code as a string and an output stream
-        for calls to 'write' - it's expected to run the code.
+def run_all_tests(runner, dir="testcases/"):
+    """Runs all tests from the directory with the given runner. A runner
+    is a function accepting Scheme code as a string and an output stream
+    for calls to 'write' - it's expected to run the code.
     """
     starttime = time.time()
     testcount = 1
@@ -50,26 +43,29 @@ def run_all_tests(runner, dir='testcases/'):
 
     for testcase in all_testcases():
         numdots = 25 - len(testcase.name)
-        dots = '.' * numdots
-        print('~~ Running test #%-3s [%s]%s.....' % (testcount, testcase.name, dots), end='')
+        dots = "." * numdots
+        print(
+            "~~ Running test #%-3s [%s]%s....." % (testcount, testcase.name, dots),
+            end="",
+        )
 
         expected = testcase.expected.lstrip()
-        ostream = StringIOUnicode()
+        ostream = io.StringIO()
         runner(testcase.code, ostream)
 
         if ostream.getvalue() == expected:
-            print('OK')
+            print("OK")
         else:
             errorcount += 1
-            print('ERROR')
-            print('-------- Expected:\n%s' % expected)
-            print('-------- Got:\n%s' % ostream.getvalue())
-            print('-------- For code:\n%s' % testcase.code)
+            print("ERROR")
+            print("-------- Expected:\n%s" % expected)
+            print("-------- Got:\n%s" % ostream.getvalue())
+            print("-------- For code:\n%s" % testcase.code)
 
         testcount += 1
 
     if errorcount == 0:
-        print('---- All tests ran OK ----')
+        print("---- All tests ran OK ----")
     else:
-        print('---- Tests had %s errors ----' % errorcount)
-    print('Elapsed: %.4s sec' % (time.time() - starttime,))
+        print("---- Tests had %s errors ----" % errorcount)
+    print("Elapsed: %.4s sec" % (time.time() - starttime,))
